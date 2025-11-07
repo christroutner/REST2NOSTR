4daa9ab7e8e25340712b564f8c837fab64901faf
Chris Troutner <chris.troutner@gmail.com>
2025-11-06 16:22:11 -0800

    Fixing issue with wlogger adapter

--- src/adapters/wlogger.js
+++ src/adapters/wlogger.js
@@ -45,6 +45,10 @@ class Wlogger {
         this.transport
       ]
     })
+
+    // Bind 'this' object to all methods
+    this.notifyRotation = this.notifyRotation.bind(this)
+    this.outputToConsole = this.outputToConsole.bind(this)
   }
 
   notifyRotation (oldFilename, newFilename) {
